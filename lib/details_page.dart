953ec279f4a522238ce43dd7d5ab88a444e7380e
Mohammad Kaycobad Jony <kaycobadjony@live.com>
2020-04-27 13:12:09 +0600

    Initial commit

---
+++ lib/details_page.dart
@@ -0,0 +1,127 @@
+import 'package:flutter/material.dart';
+
+class DetailsPage extends StatelessWidget {
+  final String imagePath;
+  final String title;
+  final String photographer;
+  final String price;
+  final String details;
+  final int index;
+  DetailsPage(
+      {@required this.imagePath,
+      @required this.title,
+      @required this.photographer,
+      @required this.price,
+      @required this.details,
+      @required this.index});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              child: Hero(
+                tag: 'logo$index',
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(30),
+                        bottomRight: Radius.circular(30)),
+                    image: DecorationImage(
+                      image: AssetImage(imagePath),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              height: 260,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(20, 20, 20, 0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          title,
+                          style: TextStyle(
+                            color: Colors.lightBlueAccent,
+                            fontSize: 22,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                        Text(
+                          'By $photographer',
+                          style: TextStyle(
+                            fontSize: 10,
+                          ),
+                        ),
+                        Text(
+                          price,
+                          style: TextStyle(
+                            color: Colors.lightBlueAccent,
+                            fontSize: 18,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          details,
+                          style: TextStyle(
+                            fontSize: 14,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: FlatButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          padding: EdgeInsets.symmetric(vertical: 15),
+                          color: Colors.lightBlueAccent,
+                          child: Text(
+                            'Back',
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 15,
+                      ),
+                      Expanded(
+                        child: FlatButton(
+                          onPressed: () {},
+                          padding: EdgeInsets.symmetric(vertical: 15),
+                          color: Colors.lightBlueAccent,
+                          child: Text(
+                            'Buy',
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/home_page.dart
@@ -0,0 +1,233 @@
+import 'package:flutter/material.dart';
+import 'details_page.dart';
+
+List<ImageDetails> _images = [
+  ImageDetails(
+    imagePath: 'images/1.jpg',
+    price: '\$20.00',
+    photographer: 'Martin Andres',
+    title: 'New Year',
+    details:
+        'This image was taken during a party in New York on new years eve. Quite a colorful shot.',
+  ),
+  ImageDetails(
+    imagePath: 'images/2.jpg',
+    price: '\$10.00',
+    photographer: 'Abraham Costa',
+    title: 'Spring',
+    details:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil error aspernatur, sequi inventore eligendi vitae dolorem animi suscipit. Nobis, cumque.',
+  ),
+  ImageDetails(
+    imagePath: 'images/3.jpg',
+    price: '\$30.00',
+    photographer: 'Jamie Bryan',
+    title: 'Casual Look',
+    details:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil error aspernatur, sequi inventore eligendi vitae dolorem animi suscipit. Nobis, cumque.',
+  ),
+  ImageDetails(
+    imagePath: 'images/4.jpg',
+    price: '\$20.00',
+    photographer: 'Jamie Bryan',
+    title: 'New York',
+    details:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil error aspernatur, sequi inventore eligendi vitae dolorem animi suscipit. Nobis, cumque.',
+  ),
+  ImageDetails(
+    imagePath: 'images/5.jpg',
+    price: '\$20.00',
+    photographer: 'Jamie Bryan',
+    title: 'New York',
+    details:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil error aspernatur, sequi inventore eligendi vitae dolorem animi suscipit. Nobis, cumque.',
+  ),
+  ImageDetails(
+    imagePath: 'images/6.jpg',
+    price: '\$20.00',
+    photographer: 'Jamie Bryan',
+    title: 'New York',
+    details:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil error aspernatur, sequi inventore eligendi vitae dolorem animi suscipit. Nobis, cumque.',
+  ),
+  ImageDetails(
+    imagePath: 'images/7.jpg',
+    price: '\$20.00',
+    photographer: 'Jamie Bryan',
+    title: 'New York',
+    details:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil error aspernatur, sequi inventore eligendi vitae dolorem animi suscipit. Nobis, cumque.',
+  ),
+  ImageDetails(
+    imagePath: 'images/8.jpg',
+    price: '\$20.00',
+    photographer: 'Jamie Bryan',
+    title: 'New York',
+    details:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil error aspernatur, sequi inventore eligendi vitae dolorem animi suscipit. Nobis, cumque.',
+  ),
+  ImageDetails(
+    imagePath: 'images/9.jpg',
+    price: '\$20.00',
+    photographer: 'Jamie Bryan',
+    title: 'New York',
+    details:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil error aspernatur, sequi inventore eligendi vitae dolorem animi suscipit. Nobis, cumque.',
+  ),
+  ImageDetails(
+    imagePath: 'images/10.jpg',
+    price: '\$20.00',
+    photographer: 'Jamie Bryan',
+    title: 'New York',
+    details:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil error aspernatur, sequi inventore eligendi vitae dolorem animi suscipit. Nobis, cumque.',
+  ),
+  ImageDetails(
+    imagePath: 'images/11.jpg',
+    price: '\$20.00',
+    photographer: 'Jamie Bryan',
+    title: 'New York',
+    details:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil error aspernatur, sequi inventore eligendi vitae dolorem animi suscipit. Nobis, cumque.',
+  ),
+  ImageDetails(
+    imagePath: 'images/12.jpg',
+    price: '\$20.00',
+    photographer: 'Jamie Bryan',
+    title: 'New York',
+    details:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil error aspernatur, sequi inventore eligendi vitae dolorem animi suscipit. Nobis, cumque.',
+  ),
+  ImageDetails(
+    imagePath: 'images/13.jpg',
+    price: '\$20.00',
+    photographer: 'Jamie Bryan',
+    title: 'New York',
+    details:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil error aspernatur, sequi inventore eligendi vitae dolorem animi suscipit. Nobis, cumque.',
+  ),
+  ImageDetails(
+    imagePath: 'images/14.jpg',
+    price: '\$20.00',
+    photographer: 'Matthew',
+    title: 'Cone Ice Cream',
+    details:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil error aspernatur, sequi inventore eligendi vitae dolorem animi suscipit. Nobis, cumque.',
+  ),
+  ImageDetails(
+    imagePath: 'images/15.jpg',
+    price: '\$25.00',
+    photographer: 'Martin Sawyer',
+    title: 'Pink Ice Cream',
+    details:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil error aspernatur, sequi inventore eligendi vitae dolorem animi suscipit. Nobis, cumque.',
+  ),
+  ImageDetails(
+    imagePath: 'images/16.jpg',
+    price: '\$15.00',
+    photographer: 'John Doe',
+    title: 'Strawberry Ice Cream',
+    details:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil error aspernatur, sequi inventore eligendi vitae dolorem animi suscipit. Nobis, cumque.',
+  ),
+];
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lightBlueAccent,
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            SizedBox(
+              height: 40,
+            ),
+            Text(
+              'Gallery',
+              style: TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.w600,
+                color: Colors.white,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            Expanded(
+              child: Container(
+                padding: EdgeInsets.symmetric(
+                  horizontal: 20,
+                  vertical: 30,
+                ),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(30),
+                    topRight: Radius.circular(30),
+                  ),
+                ),
+                child: GridView.builder(
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3,
+                    crossAxisSpacing: 10,
+                    mainAxisSpacing: 10,
+                  ),
+                  itemBuilder: (context, index) {
+                    return RawMaterialButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => DetailsPage(
+                              imagePath: _images[index].imagePath,
+                              title: _images[index].title,
+                              photographer: _images[index].photographer,
+                              price: _images[index].price,
+                              details: _images[index].details,
+                              index: index,
+                            ),
+                          ),
+                        );
+                      },
+                      child: Hero(
+                        tag: 'logo$index',
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(15),
+                            image: DecorationImage(
+                              image: AssetImage(_images[index].imagePath),
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                  itemCount: _images.length,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ImageDetails {
+  final String imagePath;
+  final String price;
+  final String photographer;
+  final String title;
+  final String details;
+  ImageDetails({
+    @required this.imagePath,
+    @required this.price,
+    @required this.photographer,
+    @required this.title,
+    @required this.details,
+  });
+}
